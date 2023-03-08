36d4cec5be8ca7a6bda86c748ac182dcd3d8969a
clbutl <cannonlbutler@gmail.com>
2023-03-07 21:43:43 -0500

    added baseline and fillers for seniors page

---
+++ src/components/SeniorComponents/Dezi.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './styles.css';
+
+import MainPic from '../../assets/MainSeniors/dezi-main.jpg'
+
+const DeziSenior = () => {
+
+
+  return (
+    <div className="individual-seniors">
+      <div className="individual-senior-img-div">
+        <img src={MainPic} alt="dezi-main"/>
+        <h1>Dezi</h1>
+      </div>
+    </div>
+  )
+}
+
+export default DeziSenior;
--- src/pages/Categories/People/Seniors/index.js
+++ src/pages/Categories/People/Seniors/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 import "./styles.css"
+import "../../subcat.css"
 import FillerImage from '../../../../assets/filler-home.png'
+import DeziSenior from "../../../../components/SeniorComponents/Dezi";
+import DeziSenior1 from "../../../../components/SeniorComponents/Dezi copy";
+import DeziSenior2 from "../../../../components/SeniorComponents/Dezi copy 2";
+import DeziSenior3 from "../../../../components/SeniorComponents/Dezi copy 3";
+import DeziSenior4 from "../../../../components/SeniorComponents/Dezi copy 4";
 
 const Seniors = () => {
 
 
   return (
-    <div>
-
+    <div className="main-subcat-div">
+      {/* Top Animals Section */}
+      <section className="subcat-top-section">
+        <div className="subcat-description-div">
+          <h1>Seniors</h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="subcat-image-div">
+          <img src={FillerImage} alt="mainAnimalPicture"/>
+        </div>
+      </section>
+      {/* Top Animals Section */}
+      <section className="subcat-middle-section">
+        <DeziSenior/>
+        <DeziSenior1/>
+        <DeziSenior2/>
+        <DeziSenior3/>
+        <DeziSenior4/>
+      </section>
     </div>
   )
 }
